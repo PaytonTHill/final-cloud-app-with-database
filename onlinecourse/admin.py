1e2698883bfa041d8f4525008d6f6451ea2fafea
PaytonTHill <132375109+PaytonTHill@users.noreply.github.com>
2023-05-07 17:36:42 -0600

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
 from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline, ]
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
+    inlines = [Question]
     extra = 5
 
 
@@ -14,13 +28,13 @@ class LessonInline(admin.StackedInline):
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
+@@ -19,12 +29,12 @@ class CourseAdmin(admin.ModelAdmin):
 
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+    inlines = [QuestionInline, ]
 
 # <HINT> Register Question and Choice models here
 
@@ -28,3 +42,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
